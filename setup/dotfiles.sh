f93943ca1fd75c202fdfdd0a81c213e748060dd1
Logan Brown <logan.brown1223@gmail.com>
2016-04-04 16:07:40 -0600

    refactor(dotfiles): force symlink

--- setup/dotfiles.sh
+++ setup/dotfiles.sh
@@ -4,7 +4,13 @@ set -e
 
 dotfiles="$HOME/.shell-config/dotfiles"
 
+if [ -e "$HOME/.gitconfig" ]
+then
+	echo ".gitconfig already exists, moving to .old_gitconfig"
+	mv $HOME/.gitconfig $HOME/.old_gitconfig
+fi
+
 # Git files
-ln -s "$dotfiles/gitconfig" "$HOME/.gitconfig"
-ln -s "$dotfiles/gitignore_global" "$HOME/.gitignore_global"
+ln -sf "$dotfiles/gitconfig" "$HOME/.gitconfig"
+ln -sf "$dotfiles/gitignore_global" "$HOME/.gitignore_global"
 ln -s "$dotfiles/screenrc" "$HOME/.screenrc"
